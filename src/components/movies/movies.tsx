0db9ef52cd7fc4ab9085bf6de6d4c491624e666d
Kellyson <Kellysondias@outlook.com>
2022-08-21 12:42:35 -0300

    Fixing the 'pagination' feature

--- src/components/movies/movies.tsx
+++ src/components/movies/movies.tsx
@@ -22,7 +22,7 @@ export const Movies:any = () => {
       fetchData()  
     }, [page])
 
-    
+    console.log("PAGE:", page)
 
     return (
         <MovieSection>
@@ -35,7 +35,7 @@ export const Movies:any = () => {
                     <div className='page'>
                         <i onClick={() => page > 1 && setPage(page - 1)} className="fa-solid fa-angle-left"></i>
                         <span>{`Página ${pageNumber} de 10`}</span>
-                        <i onClick={() => page <= 10 && setPage(page + 1)} className="fa-solid fa-angle-right"></i>
+                        <i onClick={() => page < 10 && setPage(page + 1)} className="fa-solid fa-angle-right"></i>
                     </div>
     
                     <MovieList>
@@ -60,9 +60,9 @@ export const Movies:any = () => {
                     </MovieList>
 
                     <div className='page'>
-                        <i onClick={() => page > 1 && setPage(page - 1)} className="fa-solid fa-angle-left"></i>
-                        <span>{`Página ${pageNumber} de 10`}</span>
-                        <i onClick={() => page <= 10 && setPage(page + 1)} className="fa-solid fa-angle-right"></i>
+                        <i onClick={() => page >= 1 && setPage(page - 1)} className="fa-solid fa-angle-left"></i>
+                        <span>{`Página ${pageNumber} de 11`}</span>
+                        <i onClick={() => page >= 10 && setPage(page + 1)} className="fa-solid fa-angle-right"></i>
                     </div>
                 </>
             }
